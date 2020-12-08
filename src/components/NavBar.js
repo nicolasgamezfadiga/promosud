430eb71dc228c8cfa8957e85ab0859a44ec5f3d6
nicolasgamezfadiga <56926097+nicolasgamezfadiga@users.noreply.github.com>
2020-12-08 18:12:26 -0300

    ultimp

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,9 +2,26 @@ import React from "react"
 
 const NavBar = () => {
     return(
-        <footer>
         
-        </footer>
+        <div>  
+         <h1>Promosud</h1>    
+        <nav class="navbar navbar-expand-sm bg-light navbar-light">
+        <ul class="navbar-nav">
+         <li class="nav-item active">
+        <a class="nav-link" href="#">Electronica</a>
+         </li>
+         <li class="nav-item">
+         <a class="nav-link" href="#">Libreria</a>
+        </li>
+        <li class="nav-item col-lg">
+      <a class="nav-link" href="#">Regalos</a>
+        </li>
+        <li class="nav-item">
+      <a class="nav-link" href="#">Ofertas</a>
+    </li>
+  </ul>
+</nav>
+        </div>
     )
 }
 
